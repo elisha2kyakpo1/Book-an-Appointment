03ed328348ea91fd8997430f9800b739a0ced0dc
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-10-23 21:36:36 +0200

    Fix authentication with devse

--- db/migrate/20211023183929_devise_create_clients.rb
+++ db/migrate/20211023183929_devise_create_clients.rb
@@ -18,7 +18,7 @@ class DeviseCreateClients < ActiveRecord::Migration[6.1]
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
       # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
+      # t.string   :sign_in_ip
       # t.string   :last_sign_in_ip
 
       ## Confirmable
