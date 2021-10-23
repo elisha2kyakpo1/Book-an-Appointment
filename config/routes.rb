0f85d919c31e1eda9082d3f58b41af8c95cb9e5f
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-10-23 12:41:32 +0200

    Add react app to rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/home'
+  get '/app', to: 'welcome#app', as: 'app'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
 end
