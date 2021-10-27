43d88ebe668c833c82626c84039c8c0d8c319631
Erez Friemagor <erezfree29@gmail.com>
2021-10-27 11:41:10 +0100

    change validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,5 @@ class Client < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :appointments
   has_many :doctors, through: :appointments, dependent: :destroy
-  validates :email, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 3, maximum: 40 }
 end
