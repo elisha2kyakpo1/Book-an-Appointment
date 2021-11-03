fef00b9dca385950d46b0b81779823f3b5431921
Erez Friemagor <erezfree29@gmail.com>
2021-11-03 19:50:26 +0000

    react validations

--- app/javascript/components/doctors/Dr.jsx
+++ app/javascript/components/doctors/Dr.jsx
@@ -1,59 +1,39 @@
-/* eslint-disable no-alert */
-/* eslint-disable consistent-return */
+/* eslint-disable react/no-this-in-sfc */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+/* eslint-disable no-restricted-globals */
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
 import { useSelector } from 'react-redux';
 
 const AddDoctor = () => {
   const doctors = useSelector((state) => state.doctorReducer);
+
   const onSubmit = (e) => {
     let submit = true;
     const form = new FormData();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const image = document.querySelector('input[type="file"]').files[0];
     if (name === '') {
-      document.getElementById('name_error').innerHTML = 'please enter your name';
+      document.getElementById('name_error').innerHTML = 'name cant be empty';
       submit = false;
     }
 
     if (email === '') {
-      document.getElementById('email_error').innerHTML = 'please enter your email';
-      submit = false;
-    }
-
-    if (doctors.filter((doctor) => doctor.email === email).length > 0) {
-      document.getElementById('email_error').innerHTML = 'email is already taken';
-      submit = false;
-    }
-
-    if (phone === '') {
-      document.getElementById('phone_error').innerHTML = 'please enter your phone';
-      submit = false;
-    }
-
-    if (doctors.filter((doctor) => doctor.phone === phone).length > 0) {
-      document.getElementById('phone_error').innerHTML = 'phone is already taken';
-      submit = false;
-    }
-
-    if (image === undefined) {
-      document.getElementById('email_error').innerHTML = 'please upload an image';
+      document.getElementById('email_error').innerHTML = 'email cant be empty';
       submit = false;
     }
 
     if (submit === true) {
-      form.append('name', name);
-      form.append('image', image);
-      form.append('email', email);
-      form.append('phone', phone);
+      form.append('name', document.getElementById('name').value);
+      form.append('image', document.querySelector('input[type="file"]').files[0]);
+      form.append('email', document.getElementById('email').value);
+      form.append('phone', document.getElementById('phone').value);
       form.append('about', document.getElementById('about').value);
       fetch('/api/v1/doctors', {
         method: 'POST',
         body: form,
       });
-      window.location.href = '/';
+      location.replace('http://localhost:3000/');
     }
     e.preventDefault();
   };
@@ -65,9 +45,7 @@ const AddDoctor = () => {
       <div>
         <div id="name_error" />
         <div id="email_error" />
-        <div id="phone_error" />
-        <div id="image_error" />
-        <form onSubmit={onSubmit}>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>name</label>
             <input type="text" name="name" id="name" />
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,8 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :clients, through: :appointments, dependent: :destroy
-  validates :name,:phone, presence: true, uniqueness: true, length: { minimum: 1, maximum: 50 }
+  validates :name,:phone, :email, presence: true
+  validates :phone, :email, uniqueness: true
   validates :phone, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
   validates :email, presence: true, uniqueness: true
 end
