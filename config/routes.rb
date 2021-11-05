8883d4dc927402b45d2f0a4c47c17d940acadf3a
Erez Friemagor <erezfree29@gmail.com>
2021-11-05 00:00:48 +0000

    solve routing problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'addDoctor', to: 'pages#home'
+  get 'DetailsPage/:id', to: 'pages#home'
   devise_for :clients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
