6f94f1981c8fee3a746604832ed0fafe0438311c
Erez Friemagor <erezfree29@gmail.com>
2021-11-08 14:11:18 +0000

    design doctors page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/ConfigureStore';
-import Home from './Home';
 import Doctors from './doctors/Doctors';
 import DoctorDetails from './doctors/DoctorDetails';
 import AddDoctor from './doctors/AddDoctor';
@@ -18,8 +17,7 @@ function App() {
       <Router>
         <div className="page container">
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/doctors" component={Doctors} />
+            <Route exact path="/" component={Doctors} />
             <Route path="/DetailsPage/:id" component={DoctorDetails} />
             <Route path="/doctor/appointment" component={DoctorAppointment} />
             <Route path="/addDoctor" component={AddDoctor} />
