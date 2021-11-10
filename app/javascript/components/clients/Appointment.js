ad0aa91f55aa4dedf0ad509f056b5cd69e6e93b3
Erez Friemagor <erezfree29@gmail.com>
2021-11-10 14:23:55 +0000

    allow doctor details page to refresh without losing the doctor

--- app/javascript/components/clients/Appointment.js
+++ app/javascript/components/clients/Appointment.js
@@ -1,17 +1,32 @@
 /* eslint-disable react/jsx-key */
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getClientAppointments } from '../redux/Appointments';
 
 function Appointment() {
   const clientAppointments = useSelector((state) => state.clientAppointmentReducer);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!clientAppointments.length) {
+      dispatch(getClientAppointments());
+    }
+  }, []);
 
   return (
     <div>
       <h1>My appointments</h1>
       {clientAppointments.map((appointment) => (
         <div>
-          {appointment.appointment_date}
-          {appointment.appointment_time}
+          <div>
+            Appointment Date&nbsp;&nbsp;
+            {appointment.appointment_date}
+          </div>
+          <div>
+            Time&nbsp;&nbsp;
+            {appointment.appointment_time}
+          </div>
         </div>
       ))}
     </div>
--- app/javascript/components/doctors/DoctorDetails.js
+++ app/javascript/components/doctors/DoctorDetails.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-empty */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-key */
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import DoctorAppointment from '../clients/Doctorappointment';
 import DoctorButton from './DoctorButton';
 import './Doctor.css';
 
 function DoctorDetails() {
-  const doctors = useSelector((state) => state.doctorReducer);
+  let doctors = useSelector((state) => state.doctorReducer);
   const { id } = useParams();
   const [isOpen, setIsOpen] = useState(false);
 
@@ -17,11 +18,17 @@ function DoctorDetails() {
     setIsOpen(!isOpen);
   };
 
+  if (!localStorage.getItem('stored_doctors')) {
+    localStorage.setItem('stored_doctors', JSON.stringify(doctors));
+  }
+
+  const stored_doctors = JSON.parse(localStorage.getItem('stored_doctors'));
+
   return (
     <div className="outter_container">
       <div className="contain">
         <div className="doctor">
-          {doctors.filter((doctor) => doctor.id == id).map((doc) => (
+          {stored_doctors.filter((doctor) => doctor.id == id).map((doc) => (
             <div className="doctor">
               <div className="circle">
                 <img src={doc.image} alt="doctors" />
