c2902b7a33591e3731dab3be1af1d31b0363a454
Tendai <tnyandoro@gmail.com>
2021-11-16 07:52:25 +0200

    add bar

--- app/javascript/components/menu/Sidebar.js
+++ app/javascript/components/menu/Sidebar.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Hamburger.css'
 import './Sidebar.css';
 
-const SideBar = () => (
-  <ul className="side-menu">
-    <li>
-      <Link className="link" to="/">
-        Home
-      </Link>
-    </li>
-    <li>
-      <Link className="link" to="/addDoctor">
-        Add Doctor
-      </Link>
-    </li>
-    <li>
-      <Link className="link" to="/appointments">
-        Appointment
-      </Link>
-    </li>
-    <li>
-      <Link className="link" to="/profile">
-        Profile
-      </Link>
-    </li>
-  </ul>
+const SideBar = () => {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+(
+  
+  <div className="mobile-container">
+    <ul className="side-menu">
+      <li>
+        <Link className="link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="link" to="/addDoctor">
+          Add Doctor
+        </Link>
+      </li>
+      <li>
+        <Link className="link" to="/appointments">
+          Appointment
+        </Link>
+      </li>
+      <li>
+        <Link className="link" to="/profile">
+          Profile
+        </Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default SideBar;
