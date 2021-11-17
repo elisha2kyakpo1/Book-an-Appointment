a4660ab991b73d17880147189927b1154d59d2cc
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-17 14:14:49 +0200

    Add hamburger

--- app/javascript/components/menu/Hamburger.js
+++ app/javascript/components/menu/Hamburger.js
@@ -1,41 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './Hamburger.css';
 
 const Hamburger = () => {
-  const navslide = () => {
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
-    nav.classList.toggle("nav-active");
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-      }
-    });
 
-    burger.classList.toggle("toggle");
-  };
+  const [showMenu, setShowMenu] = useState(false);
 
+  let menu;
+
+  if (showMenu) {
+    menu = <ul className="nav-links">
+      <li>
+        <Link className="link-mobile" to="/">Home</Link>
+      </li>
+      <li>
+        <Link className="link-mobile" to="/addDoctor">Add doctor</Link>
+      </li>
+      <li>
+        <Link className="link-mobile" to="/appointments">My appointment</Link>
+      </li>
+    </ul>
+  }
+  
   return (
     <div className="parent-container">
       <nav className="nav-menu-mobile d-flex">
-        <ul className="nav-links">
-          <li>
-            <Link className="link-mobile" to="/">Home</Link>
-          </li>
-          <li>
-            <Link className="link-mobile" to="/addDoctor">Add doctor</Link>
-          </li>
-          <li>
-            <Link className="link-mobile" to="/appointments">My appointment</Link>
-          </li>
-        </ul>
+        {menu}
         <div className="logo">
           <h3>LEADING DOCTORS</h3>
         </div>
-        <div className="burger" onClick={() => navslide()}>
+        <div className="burger" onClick={() => setShowMenu(!showMenu)}>
           <div className="line"></div>
           <div className="line"></div>
           <div className="line"></div>
